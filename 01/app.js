0eaecad8ef9e0a334a116dee05e71ed47b3b00a9
hatef-dev <hatef.dev21@gmail.com>
2024-12-01 16:43:44 +0330

    modify events and keyup event key modify

--- 01/app.js
+++ 01/app.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return{
             firstName: "Hatef",
+            middleName: '',
             lastName: "Sanati",
             url: "https://google.com",
             raw_html: '<a href="https://bing.com">Bing</a>',
@@ -11,13 +12,16 @@ const app = Vue.createApp({
     },
     methods: {
         fullName() {
-            return this.firstName+ " " + this.lastName.toUpperCase();
+            return `${this.firstName} ${this.middleName} ${this.lastName}`;
         },
         changeLastName(msg,e){
             console.log(msg)
             console.log(e)
             this.lastName = e.target.value;
-        }
+        },
+        addTomiddleName(e){
+            this.middleName = e.target.value
+        } 
     }
 }).mount('#app')
 
