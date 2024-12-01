2d2875ff7f28a6b5722739bc80078c7a4a9eb756
hatef-dev <hatef.dev21@gmail.com>
2024-12-01 22:39:50 +0330

    what's the diffrent about methods and computed

--- 01/app.js
+++ 01/app.js
@@ -12,16 +12,24 @@ const app = Vue.createApp({
     },
     methods: {
         fullName() {
+            console.log("it's work")
             return `${this.firstName} ${this.middleName} ${this.lastName}`;
         },
         changeLastName(msg,e){
             console.log(msg)
             console.log(e)
+            // console.log("it's work2")
             this.lastName = e.target.value;
         },
         addTomiddleName(e){
             this.middleName = e.target.value
         } 
+    },
+    computed: {
+        fullNameC() {
+            console.log("it's work from computed")
+            return `${this.firstName} ${this.middleName} ${this.lastName}`;
+        }
     }
 }).mount('#app')
 
