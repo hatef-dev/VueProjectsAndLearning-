434d2932550c172bb9ca3f14bae7049af7262c83
hatef-dev <hatef.dev21@gmail.com>
2024-12-02 18:16:41 +0330

    learn about v-for and key attr

--- 01/app.js
+++ 01/app.js
@@ -1,42 +1,43 @@
 const app = Vue.createApp({
-    data() {
-        return{
-            firstName: "Hatef",
-            middleName: '',
-            lastName: "Sanati",
-            url: "https://google.com",
-            raw_html: '<a href="https://bing.com">Bing</a>',
-            age: 22,
-            selectedOptions: ''
-            
-        }
+  data() {
+    return {
+      firstName: "Hatef",
+      middleName: "",
+      lastName: "Sanati",
+      url: "https://google.com",
+      raw_html: '<a href="https://bing.com">Bing</a>',
+      age: 22,
+      selectedOptions: "",
+      people: [
+        { name: "hatef", age: 22 },
+        { name: "haniye", age: 22 },
+      ],
+    };
+  },
+  methods: {
+    fullName() {
+      console.log("it's work");
+      return `${this.firstName} ${this.middleName} ${this.lastName}`;
     },
-    methods: {
-        fullName() {
-            console.log("it's work")
-            return `${this.firstName} ${this.middleName} ${this.lastName}`;
-        },
-        changeLastName(msg,e){
-            console.log(msg)
-            console.log(e)
-            // console.log("it's work2")
-            this.lastName = e.target.value;
-        },
-        addTomiddleName(e){
-            this.middleName = e.target.value
-        }, 
-        
+    changeLastName(msg, e) {
+      console.log(msg);
+      console.log(e);
+      // console.log("it's work2")
+      this.lastName = e.target.value;
     },
-    computed: {
-        fullNameC() {
-            console.log("it's work from computed")
-            return `${this.firstName} ${this.middleName} ${this.lastName}`;
-        }
+    addTomiddleName(e) {
+      this.middleName = e.target.value;
     },
-    watch: {
-        age(newVal, oldVal) {
-            console.log(newVal+ oldVal)      
-        }
-    }
-}).mount('#app')
-
+  },
+  computed: {
+    fullNameC() {
+      console.log("it's work from computed");
+      return `${this.firstName} ${this.middleName} ${this.lastName}`;
+    },
+  },
+  watch: {
+    age(newVal, oldVal) {
+      console.log(newVal + oldVal);
+    },
+  },
+}).mount("#app");
