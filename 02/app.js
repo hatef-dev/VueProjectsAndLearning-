5360208fcbdd08bccdcf1b87911dbe12a5f3c13b
hatef-dev <hatef.dev21@gmail.com>
2024-12-06 13:24:33 +0330

    finish project

--- 02/app.js
+++ 02/app.js
@@ -1,10 +1,32 @@
-new Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
-      perspective: 0,
+      perspective: 100,
       rotateX: 0,
       rotateY: 0,
       rotateZ: 0,
     };
   },
-}).mount('#app');
+  computed: {
+    box() {
+      return {
+        transform: `perspective(${this.perspective}px)
+             rotateX(${this.rotateX}deg)
+             rotateY(${this.rotateY}deg)
+             rotateZ(${this.rotateX}deg)`,
+      };
+    },
+  },
+  methods: {
+    reset() {
+      this.perspective = 0;
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+    },
+    async copy() {
+        const text = `transform :${this.box.transform};`
+        await navigator.clipboard.writeText(text)
+    }
+  },
+}).mount("#app");
